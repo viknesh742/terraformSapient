d06d1f76170a461ad709b20c7aef3ef52bb3761c
yashika2707 <lavanyavelu1@gmail.com>
2025-07-12 19:48:49 +0530

    update terraform cidr block

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = var.backend_bucket
+    backend_bucket      = "cf-templates-5vl2mzkujuwa-us-east-1"
     key            = "terraform.tfstate"
     encrypt        = true
   }
