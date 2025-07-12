9c20bfa21a16bdc41661300a24bd5d927da1a6bc
yashika2707 <lavanyavelu1@gmail.com>
2025-07-12 19:51:38 +0530

    removed backend conf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "s3" {
-    backend_bucket      = "cf-templates-5vl2mzkujuwa-us-east-1"
-    key            = "terraform.tfstate"
-    encrypt        = true
-  }
+  backend "s3" {}
 }
