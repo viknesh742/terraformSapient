c1f9d461ddcb6747be60f711c0c427c2078bf5c5
yashika2707 <lavanyavelu1@gmail.com>
2025-07-12 19:39:39 +0530

    update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,8 +2,6 @@ terraform {
   backend "s3" {
     bucket         = var.backend_bucket
     key            = "terraform.tfstate"
-    region         = var.aws_region
-    dynamodb_table = var.dynamodb_table
     encrypt        = true
   }
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,3 +1,4 @@
+aws_region          = "us-east-1"
 aws_profile         = "default"
 vpc_cidr            = "10.0.0.0/16"
 public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
