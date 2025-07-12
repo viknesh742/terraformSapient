8088316fad9dfaa9c2e7b2f3450232a518d69636
yashika2707 <lavanyavelu1@gmail.com>
2025-07-12 19:58:30 +0530

    update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "cf-templates-5vl2mzkujuwa-us-east-1"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform"
+  }
 }
