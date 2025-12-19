9da511a76e48d434ed96e84f0307d770714a09b7
viknesh742 <54580316+viknesh742@users.noreply.github.com>
2025-12-19 12:00:16 +0530

    Change S3 backend region from us-east-1 to us-west-2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "gpsec-test"
     key            = "dev.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-2"
     dynamodb_table = "dynamodbTablename"
   }
 }
